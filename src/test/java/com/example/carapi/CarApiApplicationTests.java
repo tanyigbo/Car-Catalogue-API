e95c91371b2b2f95046187b57d3830808c679084
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-03 14:42:39 -0500

    inital commit

---
+++ src/test/java/com/example/carapi/CarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
