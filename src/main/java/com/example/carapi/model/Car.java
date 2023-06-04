1f78a5a29c158c5b4b3bc79f1f6b9e26308ee130
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-04 07:03:00 -0500

    refactored car model removed redundent fields

--- src/main/java/com/example/carapi/model/Car.java
+++ src/main/java/com/example/carapi/model/Car.java
@@ -13,12 +13,6 @@ public class Car {
     private Long id;
 
     @Column
-    private String name;
-
-    @Column
-    private String make;
-
-    @Column
     private String model;
 
     @Column
@@ -39,32 +33,14 @@ public class Car {
     public Car() {
     }
 
-    public Car(Long id, String name, String make, String model, Manufacture manufacture) {
+    public Car(Long id, String model, Manufacture manufacture) {
         this.id = id;
-        this.name = name;
-        this.make = make;
         this.model = model;
         this.manufacture = manufacture;
     }
 
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
     }
 
     public String getModel() {
@@ -111,8 +87,6 @@ public class Car {
     public String toString() {
         return "Car{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", make='" + make + '\'' +
                 ", model='" + model + '\'' +
                 ", view_count=" + view_count +
                 ", manufacture=" + manufacture +
