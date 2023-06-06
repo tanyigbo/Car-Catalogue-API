1d73bea10c8fe4613c4660ba8c2386ebc8e66e96
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-06 02:13:45 -0500

    added @Respository decorator

--- src/main/java/com/example/carapi/repository/ImageRepository.java
+++ src/main/java/com/example/carapi/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.example.carapi.repository;
 
 import com.example.carapi.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
 }
--- src/main/java/com/example/carapi/repository/ReviewRepository.java
+++ src/main/java/com/example/carapi/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.example.carapi.repository;
 
 import com.example.carapi.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
 }
