6fc175bb788d3e142c66b7b80c5db4622506c924
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-07 01:03:46 -0500

    added endpoint for get all images of review

--- src/main/java/com/example/carapi/controller/ReviewController.java
+++ src/main/java/com/example/carapi/controller/ReviewController.java
@@ -1,7 +1,12 @@
 package com.example.carapi.controller;
 
+import com.example.carapi.model.Review;
 import com.example.carapi.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final ResponseController responseController;
 
     @Autowired
-    public ReviewController(ReviewService reviewService){
-        this.reviewService =reviewService;
+    public ReviewController(ReviewService reviewService, ResponseController responseController) {
+        this.reviewService = reviewService;
+        this.responseController = responseController;
+    }
+
+    @GetMapping(path = "/reviews/{reviewId}/images")
+    private ResponseEntity<?> getAllImagesOfReview(@PathVariable Long reviewId) {
+        try {
+            Review review = reviewService.getReviewById(reviewId);
+            return responseController.successfulRequestResponse(review.getReviewImageList(), HttpStatus.OK);
+        }catch (Exception e){
+            return responseController.failureRequestResponse(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
     }
 }
