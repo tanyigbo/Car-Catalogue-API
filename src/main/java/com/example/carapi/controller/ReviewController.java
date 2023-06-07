df0f28f40eabe43c51e317640093bc841fa4a418
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-07 04:14:54 -0500

    added logic for adding new review

--- src/main/java/com/example/carapi/controller/ReviewController.java
+++ src/main/java/com/example/carapi/controller/ReviewController.java
@@ -1,14 +1,13 @@
 package com.example.carapi.controller;
 
+import com.example.carapi.model.Car;
 import com.example.carapi.model.Review;
+import com.example.carapi.service.CarService;
 import com.example.carapi.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -24,12 +23,22 @@ public class ReviewController {
     }
 
     @GetMapping(path = "/reviews/{reviewId}/images")
-    private ResponseEntity<?> getAllImagesOfReview(@PathVariable Long reviewId) {
+    public ResponseEntity<?> getAllImagesOfReview(@PathVariable Long reviewId) {
         try {
             Review review = reviewService.getReviewById(reviewId);
             return responseController.successfulRequestResponse(review.getReviewImageList(), HttpStatus.OK);
-        }catch (Exception e){
-            return responseController.failureRequestResponse(e.getMessage(),HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return responseController.failureRequestResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(path = "reviews/new/{carId}")
+    public ResponseEntity<?> addNewReview(@PathVariable Long carId, @RequestBody Review reviewObj) {
+        try {
+            Review review = reviewService.createReview(carId, reviewObj);
+            return responseController.successfulRequestResponse(review, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return responseController.failureRequestResponse(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
--- src/main/java/com/example/carapi/service/ReviewService.java
+++ src/main/java/com/example/carapi/service/ReviewService.java
@@ -1,6 +1,7 @@
 package com.example.carapi.service;
 
 import com.example.carapi.exception.InformationNotFoundException;
+import com.example.carapi.model.Car;
 import com.example.carapi.model.Review;
 import com.example.carapi.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +13,30 @@ import java.util.Optional;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final CarService carService;
+
 
     @Autowired
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, CarService carService) {
         this.reviewRepository = reviewRepository;
+        this.carService = carService;
     }
 
-    public Review getReviewById(Long reviewId){
+    public Review getReviewById(Long reviewId) {
         Optional<Review> review = reviewRepository.findById(reviewId);
-        if(review.isPresent()){
+        if (review.isPresent()) {
             return review.get();
         }
-        throw new InformationNotFoundException("Review with ID " + reviewId +" was not found.");
+        throw new InformationNotFoundException("Review with ID " + reviewId + " was not found.");
+    }
+
+    public Review createReview(Long carId, Review reviewObj) {
+        try {
+            Car car = carService.getCarById(carId);
+            reviewObj.setCar(car);
+            return reviewRepository.save(reviewObj);
+        } catch (Exception e) {
+            throw new InformationNotFoundException("Car with ID " + carId + " was not found.");
+        }
     }
 }
