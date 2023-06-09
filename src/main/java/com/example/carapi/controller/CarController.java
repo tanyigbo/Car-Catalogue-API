532f874208d78458409c89eb142f99b1d56ee845
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 09:09:56 -0500

    car controller doc strings

--- src/main/java/com/example/carapi/controller/CarController.java
+++ src/main/java/com/example/carapi/controller/CarController.java
@@ -25,12 +25,23 @@ public class CarController {
         this.responseController = responseController;
     }
 
+    /**
+     * GET Request for all cars
+     * @return Response with a list of Cars
+     */
+    //http://localhost:8080/api/cars
     @GetMapping(path = "/cars")
     public ResponseEntity<?> getAllCars() {
         List<Car> cars = carService.getAllCars();
         return responseController.successfulRequestResponse(cars,HttpStatus.OK);
     }
 
+    /**
+     * GET Request for car with matching id
+     * @param carId id of requested Car
+     * @return Response with Car
+     */
+    //http://localhost:8080/api/cars/1
     @GetMapping(path = "/cars/{carId}")
     public ResponseEntity<?> getCarById(@PathVariable Long carId) {
         try {
@@ -41,6 +52,12 @@ public class CarController {
         }
     }
 
+    /**
+     * GET Request for Car with matching model name
+     * @param carModel model name of Car
+     * @return Response with Car
+     */
+    //http://localhost:8080/api/cars/model/Model_S
     @GetMapping(path = "/cars/model/{carModel}")
     public ResponseEntity<?> getCarByModel(@PathVariable String carModel) {
         try {
@@ -51,6 +68,12 @@ public class CarController {
         }
     }
 
+    /**
+     * GET Request for List of Images for Car
+     * @param carId id of requested Car
+     * @return Response with List of Images
+     */
+    //http://localhost:8080/api/cars/1/images
     @GetMapping(path = "/cars/{carId}/images")
     public ResponseEntity<?> getAllImagesOfCar(@PathVariable Long carId){
         try {
@@ -61,6 +84,12 @@ public class CarController {
         }
     }
 
+    /**
+     * GET Request for List of Reviews for Car
+     * @param carId id of requested Car
+     * @return Response with List of Reviews
+     */
+    //http://localhost:8080/api/cars/1/reviews
     @GetMapping(path = "/cars/{carId}/reviews")
     public ResponseEntity<?> getAllReviewsOfCar(@PathVariable Long carId){
         try{
